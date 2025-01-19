aa1c1e8a5dabf3097ae713914048781f9c91f98f
o <o@dmstfctn.net>
2025-01-19 20:49:32 +0000

    remove trailing slash in netlify url

--- themodels.js
+++ themodels.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
       "http://localhost:3000",
       "http://themodels.org",
       "https://themodels.org",
-      "https://unrivaled-daifuku-c3c7f0.netlify.app/"
+      "https://unrivaled-daifuku-c3c7f0.netlify.app"
     ]
   }
 });
