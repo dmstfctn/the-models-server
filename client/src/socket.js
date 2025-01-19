f48608d90294936cce32f7510ef5790451047d36
o <o@dmstfctn.net>
2025-01-19 21:41:26 +0000

    back to the old constant path, but don't inclued themodels in url at all

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,8 +3,9 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 //const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
 
-const URL = (window.location.href.indexOf('localhost') > -1 ) ? 'http://localhost:5040/audience' : 'https://godmode.dmstfctn.net/themodels/audience';
-const PATH = (window.location.href.indexOf('localhost') > -1 ) ? '/themodels/themodels' : '/themodels';
+const URL = (window.location.href.indexOf('localhost') > -1 ) ? 'http://localhost:5040/audience' : 'https://godmode.dmstfctn.net/audience';
+//const PATH = (window.location.href.indexOf('localhost') > -1 ) ? '/themodels/themodels' : '/themodels';
+const PATH = '/themodels/themodels'
 
 console.log('SOCKET URL: ', URL );
 console.log( 'SOCKET_PATH: ', PATH );
