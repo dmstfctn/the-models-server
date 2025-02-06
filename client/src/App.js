bcea5c3aa3dff4369229560c3c86f913dad16638
o <o@dmstfctn.net>
2025-02-06 18:15:02 +0100

    remove the ok?

--- client/src/App.js
+++ client/src/App.js
@@ -117,11 +117,12 @@ function App() {
     }}>
       <aside className="app-meta">                
         {(!isInLobby) ? <div>
-          {queueInfo.isQueued 
-            ? `Tra ${ Math.floor(queueInfo.position / 3) + 1} sketch potrai comporre la scena` 
-            : ``
-          }
-        </div> : <div>Ok!</div> }
+            {queueInfo.isQueued 
+              ? `Tra ${ Math.floor(queueInfo.position / 3) + 1} sketch potrai comporre la scena` 
+              : ``
+            }
+          </div> 
+        : (metaState !== STATES.AcceptInput) ? `` : <div>Ok!</div> }
         {(backdrop && metaState === STATES.AcceptInput) ? <div className="next-scene-info">La prossima scena si svolge <span className="where">{backdrop.phoneCategoryIt}</span>.</div> : ''} 
         { (isInLobby && timer && timer.value >= 0) ? <Timer name={timer.name} value={timer.value} total={timer.total}/> : ''}
       </aside>
