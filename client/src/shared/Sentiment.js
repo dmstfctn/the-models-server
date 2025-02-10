8f9c2be1a727de0c40da1464e2ed250e2681d571
o <o@dmstfctn.net>
2025-02-10 13:52:59 +0100

    mode sentiment thresholds

--- client/src/shared/Sentiment.js
+++ client/src/shared/Sentiment.js
@@ -1,4 +1,4 @@
 export const NEGATIVE_SENTIMENT_THRESHOLD = -2;
-export const SOLO_NEGATIVE_SENTIMENT_THRESHOLD = -9;
-export const RATING_MIN = -10;
-export const RATING_MAX = 10;
+export const SOLO_NEGATIVE_SENTIMENT_THRESHOLD = -3;
+export const RATING_MIN = -6;
+export const RATING_MAX = 6;
